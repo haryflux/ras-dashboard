275f60dd100ac6428740373d11ec9580d64bb8c7
Hari Maheshwari <146706593+haryflux@users.noreply.github.com>
2026-09-07 21:51:49 +0530

    Fix GitHub Pages SPA routing with HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import { PersonaProvider } from "./context/PersonaContext";
 import "./index.css";
 
 // App entry point. We wrap <App /> in:
-//   - BrowserRouter  -> enables URL routing
+//   - HashRouter     -> enables GitHub Pages-compatible routing
 //   - PersonaProvider -> makes the selected persona available everywhere
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/ras-dashboard">
+    <HashRouter>
       <PersonaProvider>
         <App />
       </PersonaProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
